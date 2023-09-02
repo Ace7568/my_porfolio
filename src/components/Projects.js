a4edd0c6d7d5d1c4e952ef1792e48e77ada1c935
Ace7568 <jayjoshi.2021cs@technonjr.org>
2023-09-02 12:12:22 +0530

    modified:   src/App.css 	new file:   src/assets/img/Applied.png 	new file:   src/assets/img/Apply.png 	new file:   src/assets/img/Asearch.png 	new file:   src/assets/img/BFS.png 	new file:   src/assets/img/Createjob.png 	new file:   src/assets/img/DFS.png 	new file:   src/assets/img/Dashboard.png 	new file:   src/assets/img/Dijkstra.png 	new file:   src/assets/img/Login.png 	new file:   src/assets/img/Postedjob.png 	new file:   src/assets/img/Profile.png 	new file:   src/assets/img/Visualizer.png 	new file:   src/assets/img/Walls.png 	new file:   src/assets/img/Weights.png 	deleted:    src/assets/img/project-img1.png 	deleted:    src/assets/img/project-img2.png 	deleted:    src/assets/img/project-img3.png 	modified:   src/components/ProjectCard.js 	modified:   src/components/Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,43 +1,86 @@
 import React from 'react'
-import proimg1 from '../assets/img/project-img1.png'
-import proimg2 from '../assets/img/project-img2.png'
-import proimg3 from '../assets/img/project-img3.png'
+import visualizer from '../assets/img/Visualizer.png'
+import dijkstra from '../assets/img/Dijkstra.png'
+import Asearch from '../assets/img/Asearch.png'
+import BFS from '../assets/img/BFS.png'
+import DFS from '../assets/img/DFS.png'
+import Walls from '../assets/img/Walls.png'
+import Weights from '../assets/img/Weights.png'
+import Dash from '../assets/img/Dashboard.png'
+import Apply from '../assets/img/Apply.png'
+import Profile from '../assets/img/Profile.png'
+import Posted from '../assets/img/Postedjob.png'
+import Login from '../assets/img/Login.png'
+import create from '../assets/img/Createjob.png'
 import { Col, Container, Row, Nav,Tab } from 'react-bootstrap'
 import ProjectCard from './ProjectCard'
 import colorSharp2 from '../assets/img/color-sharp2.png'
+import { App } from 'react-bootstrap-icons'
 
 export const Projects = () => {
 
-    const projects = [
+    const project1 = [
         {
             title: "PathVisualizer",
             description: "It's a react js based project which can help in visualising various path finding algorithms",
-            imgUrl: proimg1,
+            imgUrl: visualizer,
         },
         {
             title: "Dijkstra Algorithm",
             description: "We can visualize dijkstra algorithm",
-            imgUrl: proimg2,
+            imgUrl: dijkstra,
         },
         {
             title: "A* search algorithm",
             description: "A* Search algorithm is one of the best and popular technique used in path-finding and graph traversals.",
-            imgUrl: proimg3,
+            imgUrl: Asearch,
         },
         {
             title: "BFS",
             description: "The Breadth First Search (BFS) algorithm is used to search a graph data structure for a node that meets a set of criteria.",
-            imgUrl: proimg1,
+            imgUrl: BFS,
         },
         {
             title: "DFS",
             description: "Depth-first search is an algorithm for traversing or searching tree or graph data structures",
-            imgUrl: proimg1,
+            imgUrl: DFS,
         },
         {
             title: "Walls and Weights",
             description: "For weighted algorithms we can add weights and can also introduce walls",
-            imgUrl: proimg1,
+            imgUrl: Walls,
+        }
+    ]
+    const project2 = [
+        {
+            title: "Job Portal",
+            description: "Created a personal portfolio website showcasing my work, skills, and achievements, enhancing my online presence professional branding",
+            imgUrl: Dash,
+        },
+        {
+            title: "Login/Registration",
+            description: "Login/Registration facility for Recruiter/Seeker",
+            imgUrl: Login,
+        },
+        {
+            title: "Posted Job",
+            description: "A section which fetch all the posted job by Recruiter from Database",
+            imgUrl: Posted,
+        },
+        {
+            title: "Create Jobs",
+            description: "Recruiter can directly create a job which will then save in Database",
+            imgUrl: create,
+        },
+        {
+            title: "Profile",
+            description: "A profile section where user can change their name and uplode their CV's which then dynamically saves in Database",
+            imgUrl: Profile,
+        },
+        {
+            title: "Apply job",
+            description: "Seeker can apply for jobs uploded by recruiter",
+            imgUrl: Apply,
         }
     ]
   return (
@@ -64,7 +107,7 @@ export const Projects = () => {
                                 <Row>
                                 <h4>PathVisualizer</h4>
                                     {
-                                        projects.map((projects, index) => {
+                                        project1.map((projects, index) => {
                                             return(
                                                 <ProjectCard key={index} {...projects}/>
                                             )
@@ -73,9 +116,10 @@ export const Projects = () => {
                                 </Row>
                             </Tab.Pane>
                             <Tab.Pane eventKey='second'>
+                                <h4>Job Portal</h4>
                                 <Row>
                                     {
-                                        projects.map((projects, index) => {
+                                        project2.map((projects, index) => {
                                             return(
                                                 <ProjectCard key={index} {...projects}/>
                                             )
@@ -86,7 +130,7 @@ export const Projects = () => {
                             <Tab.Pane eventKey='third'>
                                 <Row>
                                     {
-                                        projects.map((projects, index) => {
+                                        project1.map((projects, index) => {
                                             return(
                                                 <ProjectCard key={index} {...projects}/>
                                             )
